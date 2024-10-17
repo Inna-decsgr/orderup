08216dfb319e06ba70626c16c051393c83b1fa25
Inna-decsgr <anjgkwl27@gmail.com>
2024-10-18 01:41:09 +0900

    Modified UserProfile model fields to allow optional input

--- order/orders/models.py
+++ order/orders/models.py
@@ -1,11 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# 사용자 모델(User)
+# 사용자 모델(UserProfile)
 class UserProfile(models.Model):
-  user = models.OneToOneField(User, on_delete=models.CASCADE)
-  phone_number = models.CharField(max_length=15)
-  address = models.TextField()
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    address = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.user.username
+  
 
 # 음식점 모델 (Restaurant)
 class Restaurant(models.Model):
