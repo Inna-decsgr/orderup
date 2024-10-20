65c93468b5a780b60827219c7a4faecc229a5800
Inna-decsgr <anjgkwl27@gmail.com>
2024-10-20 21:35:48 +0900

    Add URL for user profile update functionality

--- order/orders/urls.py
+++ order/orders/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import login_view, signup_view 
+from .views import login_view, signup_view, update_user
 
 urlpatterns = [
     path('signup/', signup_view, name='signup'),  # 회원가입 URL
     path('login/', login_view, name='login'),  # 로그인 URL
+    path('edituser/<int:user_id>/', update_user, name='edituser'), # 사용자 정보 수정 URL
 ]
