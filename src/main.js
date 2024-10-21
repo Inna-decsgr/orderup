7edb30020d0541e5593bc1a149d62ad0a89fba25
Inna-decsgr <anjgkwl27@gmail.com>
2024-10-21 20:22:27 +0900

    Enable credentials in Axios requests to include cookies

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store.js'
+import axios from 'axios';
+
+axios.defaults.withCredentials = true;  // Axios 요청에서 쿠키를 포함하도록 자격 증명 활성화
 
 // Bootstrap CSS
 import 'bootstrap/dist/css/bootstrap.min.css'
