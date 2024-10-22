24015f916646fcae9099f50770b79c08c8337c46
Inna-decsgr <anjgkwl27@gmail.com>
2024-10-22 22:52:18 +0900

    Add is_owner field and business_registration_number field to user model

--- order/orders/models.py
+++ order/orders/models.py
@@ -6,6 +6,8 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     phone_number = models.CharField(max_length=15, blank=True, null=True)
     address = models.TextField(blank=True, null=True)
+    is_owner = models.BooleanField(default=False)  
+    business_registration_number = models.CharField(max_length=50, blank=True, null=True) 
 
     def __str__(self):
         return self.user.username
