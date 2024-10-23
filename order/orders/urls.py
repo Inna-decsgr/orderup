64a5fc685c9206f26e20df2960a9bbb6454e6ea0
Inna-decsgr <anjgkwl27@gmail.com>
2024-10-23 22:19:07 +0900

    Add URL path for CSRF token retrieval

--- order/orders/urls.py
+++ order/orders/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import login_view, signup_view, update_user, get_user, delete_user
+from .views import login_view, signup_view, update_user, get_user, delete_user, csrf_token_view
 
 urlpatterns = [
     path('signup/', signup_view, name='signup'),  # 회원가입 URL
@@ -7,4 +7,5 @@ urlpatterns = [
     path('edituser/<int:user_id>/', update_user, name='edituser'), # 사용자 정보 수정 URL
     path('getuser/<int:user_id>/', get_user, name='getuser'), # 사용자 정보 조회 URL
     path('deleteuser/<int:user_id>/', delete_user, name='deleteuser'), # 사용자 정보 삭제 URL
+    path('csrftoken/', csrf_token_view, name='csrf_token_view'), # csrftoken 가져오는 URL
 ]
