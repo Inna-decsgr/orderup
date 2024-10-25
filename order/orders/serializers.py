77d9bf3e05d4546979837516a86b1ef014f7fa30
Inna-decsgr <anjgkwl27@gmail.com>
2024-10-25 23:15:31 +0900

    Add serializers for Category and Restaurant models

--- order/orders/serializers.py
+++ order/orders/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
-from .models import UserProfile
+from .models import UserProfile, Category
+from .models import Restaurant
+
 
 class UserProfileSerializer(serializers.ModelSerializer):
     user = serializers.PrimaryKeyRelatedField(read_only=True)  # user 필드를 기본 키로 설정
@@ -7,3 +9,15 @@ class UserProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserProfile
         fields = ['user', 'phone_number', 'address']
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['id', 'name']
+
+
+class RestaurantSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Restaurant
+        fields = ['name', 'address', 'phone_number', 'rating', 'owner', 'categories', 'operating_hours', 'description', 'image_url', 'delivery_fee']
