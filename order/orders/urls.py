0f213d4b2a4d77e7a6eb85a0741a0baad9dc43ba
Inna-decsgr <anjgkwl27@gmail.com>
2024-10-28 05:09:49 +0900

    Add URL pattern for updating store information and configure media file serving

--- order/orders/urls.py
+++ order/orders/urls.py
@@ -1,5 +1,7 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
-from .views import login_view, signup_view, update_user, get_user, delete_user, csrf_token_view, store_regis, my_store_view
+from .views import login_view, signup_view, update_user, get_user, delete_user, csrf_token_view, store_regis, my_store_view, update_store
 
 urlpatterns = [
     path('signup/', signup_view, name='signup'),  # 회원가입 URL
@@ -9,6 +11,10 @@ urlpatterns = [
     path('deleteuser/<int:user_id>/', delete_user, name='deleteuser'), # 사용자 정보 삭제 URL
     path('csrftoken/', csrf_token_view, name='csrf_token_view'), # csrftoken 가져오는 URL
     path('storeregis/', store_regis, name='store_regis'), # 가게 등록 URL
-    path('mystoreinfo/<int:user_id>', my_store_view, name='my_store_view'), # 내 가게 정보 조회 URL
-    
+    path('mystoreinfo/<int:user_id>/', my_store_view, name='my_store_view'), # 내 가게 정보 조회 URL
+    path('editstore/<int:store_id>/', update_store, name='update_store'), # 가게 정보 업데이트
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
