e891ebee988eeed594a3c7b997c0ced9e781100d
Inna-decsgr <anjgkwl27@gmail.com>
2024-10-28 05:08:58 +0900

    Configure URL patterns to serve media files in development

--- order/order/urls.py
+++ order/order/urls.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include  # include import
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),  # 관리자 URL
     path('order/', include('orders.urls')),  # order 앱의 urls.py 포함
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
