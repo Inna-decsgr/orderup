ac76cc8fb194ddff7c1ebf8be534e831b9f48bc7
Inna-decsgr <anjgkwl27@gmail.com>
2024-10-27 23:49:06 +0900

    Add edit button with toggle feature for editing mode

--- src/views/MyStore.vue
+++ src/views/MyStore.vue
@@ -3,19 +3,25 @@
     <div>내 가게</div>
     <button @click="gotoRegistrationStore">새 가게 등록하기</button>
   </div>
-  <div v-if="storedata">
-    <ul>
+  <div>
+    <ul v-if="storedata">
       <li v-for="(store, index) in storedata" :key="index">
-        <p>{{ index }}.</p>
-        <p>소유주: {{ store.owner }}</p>
-        <p>가게 이름: {{ store.name }}</p>
-        <p>가게 전화번호: {{ store.phone_number }}</p>
-        <p>가게 설명: {{ store.description }}</p>
-        <img :src="store.image_url" alt="가게 이미지" style="width:300px; height:200px">
-        <p>가게 주소: {{ store.address }}</p>
-        <p>카테고리: {{ store.categories[0] }}</p>
-        <p>가게 운영시간: {{ store.operating_hours }}</p>
-        <p>가게 평점: {{ store.rating }}</p>
+        <div v-if="EditNumber === index">
+          <EditStore :store="store" :cancel="handleCancel" />
+        </div>
+        <div v-else>
+          <p>{{ index }}.</p>
+          <button @click="editMode(index)">수정</button>
+          <p>소유주: {{ store.owner }}</p>
+          <p>가게 이름: {{ store.name }}</p>
+          <p>가게 전화번호: {{ store.phone_number }}</p>
+          <p>가게 설명: {{ store.description }}</p>
+          <img :src="store.image_url" alt="가게 이미지" style="width:300px; height:200px">
+          <p>가게 주소: {{ store.address }}</p>
+          <p>카테고리: {{ store.categories[0] }}</p>
+          <p>가게 운영시간: {{ store.operating_hours }}</p>
+          <p>가게 평점: {{ store.rating }}</p>
+          </div>
       </li>
     </ul>
   </div>
@@ -25,11 +31,16 @@
 <script>
 import axios from 'axios';
 import { mapGetters } from 'vuex';
+import EditStore from '../components/EditStore.vue'
 
 export default {
+  components: {
+    EditStore
+  },
   data() {
     return {
-      storedata: []
+      storedata: [],
+      EditNumber: null,
     }
   },
   computed: {
@@ -55,11 +66,7 @@ export default {
           }
         });
 
-        console.log('내 가게 가져오기 성공', response.data);
         this.storedata = response.data;
-
-        // 가게 가져오기 성공 시 반환받은 데이터 출력
-        console.log('내 가게', response.data);
       } catch (error) {
         if (error.response && error.response.status === 400) {
           this.errorMessage = error.response.data.message || '내 가게 정보를 불러오는 데 실패했습니다.';
@@ -71,6 +78,12 @@ export default {
     },
     gotoRegistrationStore() {
       this.$router.push('/registrationstore')
+    },
+    editMode(index) {
+      this.EditNumber = index;
+    },
+    handleCancel() {
+      this.EditNumber = null;
     }
   }
 }
