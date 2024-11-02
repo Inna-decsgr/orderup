5b796dd2535941cb57c203cc35fa44638f307ae7
Inna-decsgr <anjgkwl27@gmail.com>
2024-11-02 23:49:34 +0900

    Updated models to establish a relationship between OptionGroup and Menu and remove unnecessary fields

--- order/orders/models.py
+++ order/orders/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User 
 
 
 # 사용자 모델(UserProfile)
@@ -45,6 +45,7 @@ class Restaurant(models.Model):
 # 옵션 그룹 모델(OptionGroup)
 class OptionGroup(models.Model):
   name = models.CharField(max_length=100)
+  menu = models.ForeignKey('Menu', related_name='option_groups', on_delete=models.CASCADE, null=True)
 
 
   def __str__(self):
@@ -53,7 +54,7 @@ class OptionGroup(models.Model):
 
 # 옵션 항목 모델(OptionItem)
 class OptionItem(models.Model):
-  group = models.ForeignKey(OptionGroup, related_name='options', on_delete=models.CASCADE)
+  group = models.ForeignKey('OptionGroup', related_name='options', on_delete=models.CASCADE)
   name = models.CharField(max_length=100)
   price = models.DecimalField(max_digits=10, decimal_places=4, default=0)
 
@@ -68,11 +69,9 @@ class Menu(models.Model):
   name = models.CharField(max_length=100)
   description = models.TextField()
   price = models.DecimalField(max_digits=10, decimal_places=2)
-  available = models.BooleanField(default=True)
   image = models.ImageField(upload_to='menus/', null=True) 
   image_url = models.URLField(null=False, default="")
-  quantity = models.PositiveIntegerField(default=1)
-  options = models.ManyToManyField(OptionItem, related_name='menus', blank=True)
+  options = models.ManyToManyField('OptionItem', related_name='menus', blank=True)
 
 
   def __str__(self):
