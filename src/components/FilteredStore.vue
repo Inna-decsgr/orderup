4e05feb945c94390e6914438ff0ebeabdbc0ee3b
Inna-decsgr <anjgkwl27@gmail.com>
2024-11-07 23:39:00 +0900

    Implement filtered store component to display stores by selected category

---
+++ src/components/FilteredStore.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div v-if="filteredstore && filteredstore.length">
+      <div v-for="store in filteredstore" :key="store.id">
+        <h3>{{ store.name }}</h3>
+        <p>{{ store.address }}</p>
+        <p>{{ store.phone_number }}</p>
+        <p>{{ store.rating }}</p>
+        <p>{{ store.description }}</p>
+      </div>
+    </div>
+    <div v-else>
+      <p>No stores found</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    filteredstore: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
