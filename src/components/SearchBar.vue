4adcc097b38bc5c15ddaacc00b4083893e0e2c63
Inna-decsgr <anjgkwl27@gmail.com>
2024-11-08 18:14:57 +0900

    Create search input component and implement route to search result page

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div class="input-group mb-3">
+      <input 
+        type="text" 
+        class="form-control" 
+        placeholder="검색어를 입력해주세요" 
+        v-model="keyword"
+        @keyup.enter="goResultPage"
+      />
+      <button class="btn btn-outline-secondary" type="button" @click="goResultPage">검색</button>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      keyword: '',
+    }
+  },
+  methods: {
+    async goResultPage() {
+      this.$router.push({
+        path: '/searchresult',
+        query: {
+          keyword: this.keyword
+        }
+      })
+    }
+  }
+}
+</script>
