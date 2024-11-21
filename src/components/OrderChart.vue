e711ac32d441eb9cb77fc1d32bec3bd8bb7e4527
Inna-decsgr <anjgkwl27@gmail.com>
2024-11-22 02:40:29 +0900

    Update axios request URL to backend URL

--- src/components/OrderChart.vue
+++ src/components/OrderChart.vue
@@ -26,7 +26,7 @@ export default {
   methods: {
     async getChartData() {
       try {
-        const response = await axios.get('https://orderup-ct8tlwnxz-innas-projects-81fd8c82.vercel.app/order/popular_menu/');
+        const response = await axios.get('https://orderup-hvpk.onrender.com/order/popular_menu/');
         this.chartData = response.data;
         console.log(this.chartData);
         this.createChart();
