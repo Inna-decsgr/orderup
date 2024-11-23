74cda149def59553a6a96f0e2e5255f83bebbefd
Inna-decsgr <anjgkwl27@gmail.com>
2024-11-23 22:14:41 +0900

    Update DATABASES settings: Change HOST and PORT for Railway deployment

--- order/order/settings.py
+++ order/order/settings.py
@@ -114,8 +114,8 @@ DATABASES = {
         'NAME': config('DB_NAME', default='orderup'),  # 데이터베이스 이름
         'USER': config('DB_USER', default='root'),  # MySQL 사용자 이름
         'PASSWORD': config('DB_PASSWORD'),  # MySQL 비밀번호
-        'HOST': config('DB_HOST', default='mysql.railway.internal'),
-        'PORT': config('DB_PORT', default='3306'),
+        'HOST': config('DB_HOST', default='autorack.proxy.rlwy.net'),
+        'PORT': config('DB_PORT', default='20457'),
     }
 }
 
