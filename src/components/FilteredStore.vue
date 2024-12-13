2c1aa9c48cf35eaff0eeeef8de04ad698d8670ad
Inna-decsgr <anjgkwl27@gmail.com>
2024-12-13 15:33:00 +0900

    Add store like feature with specific user's liked store fetching

--- src/components/FilteredStore.vue
+++ src/components/FilteredStore.vue
@@ -1,9 +1,11 @@
 <template>
   <div>
     <div v-if="filteredstore && filteredstore.length">
-      <div v-for="store in filteredstore" :key="store.id" style="cursor:pointer">
-        <h3 @click="detailstore({id:store.id, name: store.name})">{{ store.name }}</h3>
-        <StoreLike :storeid="store.id"/>
+      <div v-for="store in filteredstore" :key="store.id" >
+        <h3 @click="detailstore({id:store.id, name: store.name})" style="cursor:pointer">
+          {{ store.name }}
+        </h3>
+        <StoreLike :storeid="store.id" :likedstore="this.likedstore" />
         <p>{{ store.address }}</p>
         <p>{{ store.phonenumber }}</p>
         <p>{{ store.rating }}</p>
@@ -15,6 +17,8 @@
 
 <script>
 import StoreLike from '../components/StoreLike.vue'
+import { mapGetters } from 'vuex';
+import axios from 'axios';
 
 export default {
   components: {
@@ -26,7 +30,23 @@ export default {
       required: true
     }
   },
+  computed: {
+    ...mapGetters(['getUser']),
+    user() {
+      return this.getUser;
+    },
+  },
+  mounted() {
+    this.getStoreLike()
+  },
   methods: {
+    async getStoreLike() {
+      const stores = await axios.get(`http://localhost:8000/order/getstorelikes/${this.user.id}/`, { storeid: this.storeid });
+      console.log(stores.data);
+      this.likedstore = stores.data.likes.map(like => like.store_id);
+      console.log('가게 아이디들', this.likedstore);
+      
+    },
     detailstore(store) {
       this.$router.push('/detailstore'),
       this.$store.commit('setStore', store);
