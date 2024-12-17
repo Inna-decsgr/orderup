f6f57bf82c095dc8573f93f4104bc704c55719a7
Inna-decsgr <anjgkwl27@gmail.com>
2024-12-17 15:29:05 +0900

    Fix search result filtering by adding null check for store name

--- src/views/SearchResult.vue
+++ src/views/SearchResult.vue
@@ -35,12 +35,11 @@ export default {
     async getSearchResult() {
       try {
         const response = await axios.get('http://localhost:8000/order/getallstores/');
-        console.log('검색 결과', response.data);
         this.storeData = response.data;
 
-        // 키워드 전체 문자열을 포함하는 가게들만 저장
-        this.filteredStore = this.storeData.filter((store) => store.name.includes(this.keyword));
-        console.log(this.filteredStore);
+        this.filteredStore = this.storeData.filter((store) => {
+          return store.name && store.name.includes(this.keyword);
+        })
       } catch (error) {
         if (error.response) {
           console.log("API error:", error.response.data); // 오류 메시지 출력
