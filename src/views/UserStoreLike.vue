f06bb4a761d3fc5ae14bc8a46a6a5022002532af
Inna-decsgr <anjgkwl27@gmail.com>
2025-01-01 18:25:03 +0900

    Implement feature to display all liked stores with detailed information for recommending popular stores to users

--- src/views/UserStoreLike.vue
+++ src/views/UserStoreLike.vue
@@ -1,16 +1,31 @@
 <template>
   <div>
-    <p><strong>찜한 가게</strong></p>
-    <p>총 {{ this.likedstores.length }}개</p>
-    <div v-for="store in this.likedstores" :key="store.id" @click="gotoStoreDetail(store)" class="store_item">
-      <div class="store_image">
-        <img :src="store.image_url" alt="가게 이미지" style="width: 250px; height: 200px;">
+    <div>
+      <p><strong>찜한 가게</strong></p>
+      <p>총 {{ this.userlikedstores.length }}개</p>
+      <div v-for="store in this.userlikedstores" :key="store.id" @click="gotoStoreDetail(store)" class="store_item">
+        <div class="store_image">
+          <img :src="store.image_url" alt="가게 이미지" style="width: 250px; height: 200px;">
+        </div>
+        <div class="store_info">
+          <p><strong>{{ store.store_name}}</strong></p>
+          <p>⭐ {{ store.rating }} ({{ store.review_count }})</p>
+          <p class="store_description">{{ store.description }}</p>
+          <p>배달팁 {{ store.delivery_fee }}원</p>
+        </div>
       </div>
-      <div class="store_info">
-        <p><strong>{{ store.store_name}}</strong></p>
-        <p>⭐ {{ store.rating }} ({{ store.review_count }})</p>
-        <p class="store_description">{{ store.description }}</p>
-        <p>배달팁 {{ store.delivery_fee }}원</p>
+    </div>
+    <div>
+      <p><strong>찜 많은 가게를 추천해요</strong></p>
+      <div v-for="store in likedallstores" :key="store.id" class="liked_store_item">
+        <div class="liked_store_image">
+          <img :src="store.store.image_url" alt="가게 이미지">
+        </div>
+        <div class="liked_store_info">
+          <p class="liked_store_name"><strong>{{ store.store.name }}</strong> <span>⭐ {{ store.store.rating }}</span></p>
+          <p class="liked_store_address">{{ store.store.address }}</p>
+          <p class="liked_store_delivery">배달팁 : {{ Number(store.store.delivery_fee).toLocaleString() }}원</p>
+        </div>
       </div>
     </div>
   </div>
@@ -23,7 +38,8 @@ import { mapGetters } from 'vuex';
 export default {
   data() {
     return {
-      likedstores : []
+      userlikedstores: [],
+      likedallstores: []
     }
   },
   computed: {
@@ -33,19 +49,27 @@ export default {
     },
   },
   mounted() {
-    this.getStoreLike()
+    this.getStoreLike();
+    this.getAllLikedStore();
   },
   methods: {
     async getStoreLike() {
+      // 사용자가 찜한 가게들 가져오기
       const response = await axios.get(`http://localhost:8000/order/getstorelikes/${this.user.id}/`);
       console.log(response.data);
-      this.likedstores = response.data.likes;
-      console.log('사용자가 찜한 가게들', this.likedstores);
+      this.userlikedstores = response.data.likes;
+      console.log('사용자가 찜한 가게들', this.userlikedstores);
     },
     gotoStoreDetail(store) {
       this.$router.push('/detailstore');
       this.$store.commit('setStore', store);
       console.log(store);
+    },
+    async getAllLikedStore() {
+      // 찜된 모든 가게들 가져오기
+      const response = await axios.get(`http://localhost:8000/order/getlikedallstores/`);
+      console.log('찜된 모든 가게들', response.data.liked_stores);
+      this.likedallstores = response.data.liked_stores
     }
   }
 }
@@ -109,5 +133,36 @@ export default {
   overflow: hidden;
   text-overflow: ellipsis;
   white-space: normal;
+}
+
+.liked_store_item {
+  display: flex; 
+  align-items: center; 
+  margin-bottom: 20px;
+  border-bottom: 1px solid #ddd; 
+  padding-bottom: 10px;
+}
+
+.liked_store_image img {
+  width: 100px;
+  height: 100px;
+  border-radius: 8px; 
+  object-fit: cover;
+  margin-right: 15px; 
+}
+
+.liked_store_info {
+  flex: 1;
+}
+
+.liked_store_name {
+  font-size: 14px;
+  color: #666;
+  margin-bottom: 5px;
+}
+
+.liked_store_delivery {
+  font-size: 14px;
+  color: #333;
 }
 </style>
