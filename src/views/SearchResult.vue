0070073010be5d91bc0cad45a750161e9d6f4be1
Inna-decsgr <anjgkwl27@gmail.com>
2025-01-16 15:20:48 +0900

    Update search feature to fetch and display stores with menus matching the keyword without duplicates

--- src/views/SearchResult.vue
+++ src/views/SearchResult.vue
@@ -1,7 +1,14 @@
 <template>
   <div>
-    검색 결과 페이지
-    <p>'{{ keyword }}'(으)로 검색된 결과</p>
+    <div>
+      <button @click="gotoHome"><i class="fa-solid fa-arrow-left"></i></button>
+      <input 
+        type="text" 
+        v-model="keyword"
+        @keyup.enter="searchstores"
+        class="border ml-3 pl-2 font-bold"
+      >
+    </div>
     <div v-if="filteredStore">
       <SearchFiltered :filteredstore="filteredStore"/>
     </div>
@@ -20,33 +27,46 @@ export default {
     return {
       storeData: [],
       stores: [],
-      filteredStore: []
-    }
-  },
-  computed: {
-    keyword() {
-      return this.$route.query.keyword;
+      filteredStore: [],
+      keyword: this.$route.query.keyword || '',
     }
   },
   mounted() {
-    this.getSearchResult();
+    this.getStoreMenus();
   },
   methods: {
-    async getSearchResult() {
+    async getStoreMenus() {
       try {
-        const response = await axios.get('http://localhost:8000/order/getallstores/');
-        this.storeData = response.data;
-
-        this.filteredStore = this.storeData.filter((store) => {
-          return store.name && store.name.includes(this.keyword);
-        })
-      } catch (error) {
-        if (error.response) {
-          console.log("API error:", error.response.data); // 오류 메시지 출력
-        } else {
-          console.log("Error occurred:", error.message);
+        if (!this.keyword.trim()) {
+          alert('검색어를 입력하세요.');
+          return;
         }
+
+        const response = await axios.get(`http://localhost:8000/order/getallmenus/${this.keyword}/`);
+        const restaurants = response.data.map(menu => menu.restaurant);
+        // 가게 아이디를 기준으로 중복된 가게는 제외하고 필터링
+        const uniqueRestaurants = restaurants.filter(
+          (restaurant, index, self) =>
+            index === self.findIndex(r => r.id === restaurant.id)
+        );
+
+        console.log('중복 제거된 레스토랑', uniqueRestaurants);
+        this.filteredStore = uniqueRestaurants;
+      } catch (error) {
+        console.error('메뉴 데이터를 가져오는 중 에러 발생:', error);
+        return [];
       }
+    },
+    async searchstores() {
+      await this.getStoreMenus();
+    },
+    gotoHome() {
+      this.$router.push('/')
+    },
+    filterStores(keyword) {
+      this.filteredStore = this.storeData.filter((store) => {
+        return store.name && store.name.includes(keyword);
+      })
     }
   }
 }
