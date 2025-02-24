15c5c544c47e44f3aee8e213499014e462c76895
Inna-decsgr <anjgkwl27@gmail.com>
2025-02-24 17:20:32 +0900

    feat: Improve login page styling and add footer

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -1,38 +1,42 @@
 <template>
-  <div>
-    <p>로그인 페이지</p>
-    <p>로그인</p>
-    <form @submit.prevent="handleLogin">
-      <div>
-        <label for="logininput" class="form-label">아이디</label>
-        <input 
-          type="email" 
-          class="form-control"
-          v-model="loginid"
-          id="logininput" 
-          placeholder="email@email.com"
-          required
-        >
+  <div class="flex flex-col items-center justify-center h-screen">
+    <div class="border py-[60px] px-[40px] rounded-md">
+      <div class="relative flex justify-between items-center w-[350px]">
+        <button @click="gotohome" class="absolute text-2xl">
+          <i class="fa-solid fa-arrow-left"></i>
+        </button>
+        <p class="absolute font-bold text-3xl right-[30%]">Orderup</p>
       </div>
-      <div>
-        <label for="passwordinput" class="col-form-label">비밀번호</label><span class="formtext">
-          8-20자 이내여야 합니다.
-        </span>
-        <input 
-          type="password" 
-          id="passwordinput" 
-          class="form-control"
-          v-model="loginpassword"
-          aria-describedby="passwordHelpInline" 
-          placeholder="비밀번호"
-          required
-        >
-      </div>
-      <button type="submit">로그인</button>
-      <p>아직 계정이 없으신가요?</p>
-      <button @click.prevent="gotoSignUp">회원가입</button>
-      <p v-if="errorMessage">{{ errorMessage }}</p>
-    </form>
+      <form @submit.prevent="handleLogin">
+        <div class="mt-[50px]">
+          <input 
+            type="email" 
+            class="border rounded-md w-[350px] outline-none p-2 mb-2 text-sm"
+            v-model="loginid"
+            placeholder="이메일을 입력해주세요"
+            required
+          >
+        </div>
+        <div class="mb-4">
+          <input 
+            type="password"
+            class="border rounded-md w-[350px] outline-none p-2 text-sm"
+            v-model="loginpassword"
+            placeholder="비밀번호를 입력해주세요"
+            required
+          >
+        </div>
+        <button type="submit" class="w-full bg-violet-500 p-2 rounded-md mb-[50px] font-bold text-white">로그인</button>
+        <div class="text-center text-sm flex justify-between items-center">
+          <p class="border-b-[1px] pb-2 border-violet-500">아직 계정이 없으신가요?</p>
+          <button @click.prevent="gotoSignUp" class="border py-[6px] px-3 rounded-md bg-violet-500 text-white font-bold">회원가입</button>
+        </div>
+      </form>
+    </div>
+    <div class="text-xs text-gray-500 py-[70px] text-center">
+      <p>이용약관 | 개인정보처리방침 | 책임의 한계와 법적고지 | 회원정보 고객센터 </p>
+      <p><span class="text-lg font-bold">Orderup</span> <span class="text-xs text-gray-500">Copyright Orderup Corp. All Rights Reserved.</span></p>
+    </div>
   </div>
 </template>
 
@@ -63,15 +67,12 @@ export default {
           headers: {
             'X-CSRFToken': csrfToken,  // 수정된 부분
           }
-        });
-
-        console.log('로그인 성공', response.data);
+        })
+        console.log('로그인 성공', response.data.user.username);
 
         // 로그인 성공 시 Vuex에 사용자 정보 저장
         const userData = response.data.user;
         await this.login(userData);
-
-        alert(`안녕하세요. ${userData.username}님!`)
         this.$router.push('/');
       } catch (error) {
         if (error.response && error.response.status === 400) {
@@ -84,6 +85,9 @@ export default {
     },
     gotoSignUp() {
       this.$router.push('/signup');
+    },
+    gotohome() {
+      this.$router.push('/')
     }
   }
 }
