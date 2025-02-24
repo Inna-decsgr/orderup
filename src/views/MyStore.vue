6c8e4a3ae40b06b4592d36eb72e84c818d76d2a9
Inna-decsgr <anjgkwl27@gmail.com>
2025-02-24 21:31:25 +0900

    refactor: move existing content from mystore to allmystores and add main menu

--- src/views/MyStore.vue
+++ src/views/MyStore.vue
@@ -1,83 +1,70 @@
 <template>
-  <div>
-    <div>
-      <div>내 가게</div>
-      <button @click="gotoRegistrationStore">새 가게 등록하기</button>
+  <div class="p-3">
+    <div class="flex justify-between items-center mb-2">
+      <p class="text-xl font-bold cursor-pointer" @click="gomystores">오더업 <i class="fa-solid fa-store"></i> 사장님광장</p>
     </div>
-    <div>
-      <ul v-if="storedata">
-        <li v-for="(store, index) in storedata" :key="index">
-          <div>
-            <p>{{ index }}.</p>
-            <button @click="gotoManageStore({store, index})">가게 관리하기</button>
-            <p>소유주: {{ store.owner }}</p>
-            <p>가게 이름: {{ store.name }}</p>
-            <p>가게 전화번호: {{ store.phone_number }}</p>
-            <p>가게 설명: {{ store.description }}</p>
-            <img :src="store.image_url" alt="가게 이미지" style="width:300px; height:200px">
-            <p>가게 주소: {{ store.address }}</p>
-            <p>카테고리: {{ store.categories[0] }}</p>
-            <p>가게 운영시간: {{ store.operating_hours }}</p>
-            <p>가게 평점: {{ store.rating }}</p>
-            <p>배달료: {{ store.delivery_fee }}</p>
-          </div>
-        </li>
-      </ul>
+    <div v-if="route.path === '/mystore'" class="mt-5">
+      <div class="text-center py-4 border rounded-md">
+        <p class="font-bold text-center pb-3 text-sm">전체 메뉴</p>
+        <div>
+          <button 
+            v-for="menu in menus.slice(0, 4)" 
+            :key="menu.id" 
+            class="w-[80px] h-[90px]"
+            @click="menu.click"
+          >
+            <span class="bg-[rgb(240,239,239)] text-3xl p-[10px] w-[70px] h-[70px] inline-flex justify-center items-center rounded-3xl" v-html="menu.icon"></span>
+            <p class="font-bold mt-[3px] text-xs">{{ menu.name }}</p>
+          </button>
+        </div>
+        <div class="mt-[15px]">
+          <button
+            v-for="menu in menus.slice(4, 8)"
+            :key="menu.id"
+            class="w-[80px] h-[90px]"
+          >
+            <span class="bg-[rgb(240,239,239)] text-3xl p-[10px] w-[70px] h-[70px] inline-flex justify-center items-center rounded-3xl" v-html="menu.icon"></span>
+            <p class="font-bold mt-[3px] text-xs">{{ menu.name }}</p>
+          </button>
+        </div>
+      </div>
     </div>
+    <router-view></router-view>
   </div>
 </template>
 
 
 <script>
-import axios from 'axios';
-import { mapGetters } from 'vuex';
-
+import { useRoute } from 'vue-router';
 
 export default {
   data() {
     return {
-      storedata: []
+      route: useRoute(),
+      menus: [
+        { id: 1, name: '전체현황', icon: '<i class="fa-solid fa-calendar"></i>', click: this.goallmystores },
+        { id: 2, name: '가게등록', icon: '<i class="fa-solid fa-store"></i>', click: this.gotoRegistrationStore},
+        { id: 3, name: '리뷰관리', icon: '<i class="fa-solid fa-comment"></i>', click: this.goallmyreviews },
+        { id: 4, name: '공지·한마디', icon: '<i class="fa-solid fa-pencil"></i>' },
+        { id: 5, name: '메뉴관리', icon: '<i class="fa-solid fa-rectangle-list"></i>' },
+        { id: 6, name: '메뉴품질', icon: '<i class="fa-solid fa-trash"></i>' },
+        { id: 7, name: '광고관리', icon: '<i class="fa-solid fa-flag"></i>' },
+        { id: 8, name: '광고정기결제', icon: '<i class="fa-solid fa-circle-check"></i>' },
+      ],
     }
   },
-  computed: {
-    ...mapGetters(['getUserId']),
-    userid() {
-      return this.getUserId;
-    },
-  },
-  mounted() {
-    this.getMyStore();
-  },
   methods: {
-    async getMyStore() {
-      try {
-        // CSRF 토큰 요청
-        const csrfResponse = await axios.get("http://localhost:8000/order/csrftoken/");
-        const csrfToken = csrfResponse.data.csrfToken;
-
-        const response = await axios.get(`http://localhost:8000/order/mystoreinfo/${this.userid}`, {
-          headers: {
-            'X-CSRFToken': csrfToken,  
-          }
-        });
-
-        this.storedata = response.data;
-        console.log('가게들', this.storedata);
-      } catch (error) {
-        if (error.response && error.response.status === 400) {
-          this.errorMessage = error.response.data.message || '내 가게 정보를 불러오는 데 실패했습니다.';
-        } else {
-          this.errorMessage = '서버와의 통신에 문제가 발생했습니다.';
-        }
-        console.error('내 가게 가져오기 실패', error);
-      }
-    },
     gotoRegistrationStore() {
       this.$router.push('/registrationstore')
     },
-    gotoManageStore({ store }) {
-      this.$router.push('/manageStore');
-      this.$store.commit('setStore', store);
+    goallmystores() {
+      this.$router.push('/mystore/allmystores')
+    },
+    goallmyreviews() {
+      this.$router.push('/allmyreviews')
+    },
+    gomystores() {
+      this.$router.push('/mystore')
     }
   }
 }
