591ba06c8bf6779d557fd6b28996e4f1ad4c77c3
Inna-decsgr <anjgkwl27@gmail.com>
2025-02-27 23:24:39 +0900

    Refactor: Improve menu edit component structure and styling in ManageStore

--- src/components/MyMenu.vue
+++ src/components/MyMenu.vue
@@ -1,28 +1,35 @@
 <template>
   <div>
-    <div>
-      <p>메뉴판</p>
-      <button @click="ShowForm">+메뉴 등록</button>
-      <button @click="cancel">취소</button>
+    <div class="relative">
+      <div class="flex items-center">
+        <p class="font-bold text-sm">단품 메뉴</p>
+        <button @click="ShowForm" class="absolute right-0 bg-blue-500 text-white p-2 rounded-sm ml-auto text-xs hover:bg-blue-600">메뉴 추가</button>
+      </div>
       <NewMenu v-if="this.NewForm" :cancel="cancel" :storeid="store.id" :getmenu="fetchStoreMenu" />
       <div v-for="(menu, index) in menus" :key="index">
         <div v-if="EditNumber === index">
           <EditMenu :menu="menu" :canceledit="cancelEdit" :getMenu="fetchStoreMenu"/>
         </div>
-        <div v-else>
-          <button @click="editmode(index)">메뉴 수정</button>
-          <button @click="deletemenu(menu.id)">메뉴 삭제</button>
-          <p>{{ menu.name }}</p>
-          <p>{{ menu.description }}</p>
-          <p>가격 {{ Math.floor(menu.price) }}원</p>
-          <img :src="menu.image_url" alt="Menu Image" style="width: 300px; height: 200px;" />
-          <div v-for="optionGroup in menu.option_groups" :key="optionGroup.group_name">
-            <p>{{ optionGroup.group_name }}</p>
-            <ul>
-              <li v-for="item in optionGroup.items" :key="item.name">
-                {{ item.name }} - {{ Math.floor(item.price) }}
-              </li>
-            </ul>
+        <div v-else class="flex justify-center border-b pt-3">
+          <div class="flex basis-4/5 py-3 pr-2">
+            <img :src="menu.image_url" alt="Menu Image" class="w-[80px] h-[80px] object-contain rounded-l pr-3" />
+            <div class="text-sm">
+              <p class="font-bold">{{ menu.name }}</p>
+              <p class="text-xs py-1">{{ menu.description }}</p>
+              <p class="text-xs font-bold">{{menu.price.toLocaleString() }}원</p>
+              <div v-for="optionGroup in menu.option_groups" :key="optionGroup.group_name">
+                <p>{{ optionGroup.group_name }}</p>
+                <ul>
+                  <li v-for="item in optionGroup.items" :key="item.name">
+                    {{ item.name }} - {{ Math.floor(item.price) }}
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="flex justify-center items-center basis-1/5">
+            <button @click="editmode(index)" class="bg-gray-100 rounded-sm w-[50px] mr-1 h-[30px] text-xs hover:bg-gray-200">수정</button>
+            <button @click="deletemenu(menu.id)" class="bg-gray-100 rounded-sm w-[50px] h-[30px] text-xs hover:bg-gray-200">삭제</button>
           </div>
         </div>
       </div>
