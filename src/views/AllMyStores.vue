2209c5453446bb215af87c68811037451a10ab1e
Inna-decsgr <anjgkwl27@gmail.com>
2025-02-27 22:15:27 +0900

    Refactor: Improved Allmystores component structure and styling

--- src/views/AllMyStores.vue
+++ src/views/AllMyStores.vue
@@ -1,28 +1,29 @@
 <template>
   <div>
-    <div class="py-3">
-      <p class="font-bold pb-3">내 가게</p>
-      <ul v-if="storedata">
-        <li v-for="(store, index) in storedata" :key="index" class="border p-3 rounded-md mb-4">
+    <div class="w-[430px] mx-auto">
+      <ul v-if="storedata" class="space-y-12">
+        <li v-for="(store, index) in storedata" :key="index" class="relactive border rounded-md pb-[100px]">
           <div class="text-sm">
-            <div class="flex justify-between items-center mb-2">
-              <p><span class="font-bold">{{ index+1 }}번째 가게</span></p>
-              <button @click="gotoManageStore({store, index})" class="bg-violet-500 text-white py-1 px-2 rounded-md text-sm">가게 관리</button>
+            <div class="relative items-center mb-10">
+              <img :src="imageSrc(store.image_url)" alt="가게 이미지" class="w-full h-auto aspect-[4/3] object-cover rounded-md">
+              <div class="absolute top-[90%] left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[400px] h-[300px] rounded-md bg-white text-center shadow-lg p-4">
+                <p class="font-bold text-[20px]">{{ store.name }}</p>
+                <p class="font-bold text-xs pt-1">{{ generatecode(index)}} - {{ store.categories[0] }}</p>
+                <div class="flex justify-center space-x-3 mt-2">
+                  <p><i class="fa-solid fa-star mr-1 text-yellow-500"></i>{{ store.rating }}</p>
+                  <p><i class="fa-solid fa-clock mr-1 text-gray-500"></i>{{ store.operating_hours }}</p>
+                </div>
+                <div class="flex justify-center space-x-3 mb-2 mt-1">
+                  <p><i class="fa-solid fa-phone mr-1 text-blue-600"></i>{{ store.phone_number }}</p>
+                  <p><span class="mr-1 font-bold">리뷰</span>{{ store.reviews }}</p>
+                </div>
+                <p class="mb-2"><i class="fa-solid fa-location-dot mr-1 text-red-500"></i>{{ store.address }}</p>
+                <p>{{ store.description }}</p>
+                <div class="mt-3">
+                  <button @click="gotoManageStore({store, index})" class="bg-gray-100 w-[150px] p-2 rounded-md text-sm hover:bg-gray-200 font-bold">가게 관리</button>
+                </div>
+              </div>
             </div>
-            <span class="font-bold">* 상호 대표 이미지</span>
-            <div class="my-2">
-              <img :src="imageSrc(store.image_url)" alt="가게 이미지" class="w-[200px] h-[150px] rounded-md">
-            </div>
-            <p><span class="font-bold">소유주</span> {{ store.owner }}</p>
-            <p><span class="font-bold">상호명</span> {{ store.name }}</p>
-            <p><span class="font-bold">전화번호</span> {{ store.phone_number }}</p>
-            <p class="font-bold">대표 설명</p>
-            <p>{{ store.description }}</p>
-            <p><span class="font-bold">주소</span> {{ store.address }}</p>
-            <p><span class="font-bold">분류</span> {{ store.categories[0] }}</p>
-            <p><span class="font-bold">운영시간</span> {{ store.operating_hours }}</p>
-            <p><span class="font-bold">평점</span> {{ store.rating }}</p>
-            <p><span class="font-bold">배달료</span> {{ store.delivery_fee }}원</p>
           </div>
         </li>
       </ul>
@@ -91,13 +92,28 @@ export default {
       // `http://` 또는 `https://`로 시작하면 그대로 사용
       if (imageurl.startsWith("http://") || imageurl.startsWith("https://")) {
         // 만약 "http://localhost:8000/media/"로 시작하면 "/images/" 추가
+        if (imageurl.startsWith("http://localhost:8000/media/images")) {
+          return imageurl; // 기존 URL 유지
+        }
         if (imageurl.startsWith("http://localhost:8000/media/")) {
           return imageurl.replace("/media/", "/media/images/");
         }
-        return imageurl; // 기존 URL 유지
+        return imageurl
       }
       // 상대 경로라면 `http://localhost:8000`을 붙여서 반환
       return `http://localhost:8000${imageurl}`;
+    },
+    generatecode(number) {
+      // 공통된 문자열을 기반으로 랜덤한 코드 리스트 생성
+      const prefix = 'WEKRJLD'
+
+      // 한 자리 숫자일 경우 앞에 0을 붙이기
+      const formattedNumber = String(number).padStart(2, '0'); 
+
+      // 최종 코드 생성
+      const unique_code = prefix + formattedNumber
+
+      return unique_code
     }
   }
 }
