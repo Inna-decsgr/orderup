ff387438effb8cf4baeb4b68c930efcb47ad1227
Inna-decsgr <anjgkwl27@gmail.com>
2025-03-05 17:50:45 +0900

    Handle empty menu case to prevent errors

--- src/components/MyMenu.vue
+++ src/components/MyMenu.vue
@@ -6,30 +6,32 @@
         <button @click="ShowForm" class="absolute right-0 bg-blue-500 text-white p-2 rounded-sm ml-auto text-xs hover:bg-blue-600">메뉴 추가</button>
       </div>
       <NewMenu v-if="this.NewForm" :cancel="cancel" :storeid="store.id" :getmenu="fetchStoreMenu" />
-      <div v-for="(menu, index) in menus" :key="index">
-        <div v-if="EditNumber === index">
-          <EditMenu :menu="menu" :canceledit="cancelEdit" :getMenu="fetchStoreMenu"/>
-        </div>
-        <div v-else class="flex justify-center border-b pt-3">
-          <div class="flex basis-4/5 py-3 pr-2">
-            <img :src="menu.image_url" alt="Menu Image" class="w-[80px] h-[80px] object-contain rounded-l pr-3" />
-            <div class="text-sm">
-              <p class="font-bold">{{ menu.name }}</p>
-              <p class="text-xs py-1">{{ menu.description }}</p>
-              <p class="text-xs font-bold">{{menu.price.toLocaleString() }}원</p>
-              <div v-for="optionGroup in menu.option_groups" :key="optionGroup.group_name">
-                <p class="mt-3 mb-1 text-xs text-gray-500">{{ optionGroup.group_name }}</p>
-                <ul class="text-xs">
-                  <li v-for="item in optionGroup.items" :key="item.name">
-                    {{ item.name }} - {{ item.price.toLocaleString() }}원
-                  </li>
-                </ul>
+      <div v-if="menus.message !== '등록된 메뉴가 없습니다'">
+        <div v-for="(menu, index) in menus" :key="index">
+          <div v-if="EditNumber === index">
+            <EditMenu :menu="menu" :canceledit="cancelEdit" :getMenu="fetchStoreMenu"/>
+          </div>
+          <div class="flex justify-center border-b pt-3">
+            <div class="flex basis-4/5 py-3 pr-2">
+              <img :src="menu.image_url" alt="Menu Image" class="w-[80px] h-[80px] object-contain rounded-l pr-3" />
+              <div class="text-sm">
+                <p class="font-bold">{{ menu.name }}</p>
+                <p class="text-xs py-1">{{ menu.description }}</p>
+                <p class="text-xs font-bold">{{menu.price.toLocaleString() }}원</p>
+                <div v-for="optionGroup in menu.option_groups" :key="optionGroup.group_name">
+                  <p class="mt-3 mb-1 text-xs text-gray-500">{{ optionGroup.group_name }}</p>
+                  <ul class="text-xs">
+                    <li v-for="item in optionGroup.items" :key="item.name">
+                      {{ item.name }} - {{ item.price.toLocaleString() }}원
+                    </li>
+                  </ul>
+              </div>
               </div>
             </div>
-          </div>
-          <div class="flex justify-center items-center basis-1/5">
-            <button @click="editmode(index)" class="bg-gray-100 rounded-sm w-[50px] mr-1 h-[30px] text-xs hover:bg-gray-200">수정</button>
-            <button @click="deletemenu(menu.id)" class="bg-gray-100 rounded-sm w-[50px] h-[30px] text-xs hover:bg-gray-200">삭제</button>
+            <div class="flex justify-center items-center basis-1/5">
+              <button @click="editmode(index)" class="bg-gray-100 rounded-sm w-[50px] mr-1 h-[30px] text-xs hover:bg-gray-200">수정</button>
+              <button @click="deletemenu(menu.id)" class="bg-gray-100 rounded-sm w-[50px] h-[30px] text-xs hover:bg-gray-200">삭제</button>
+            </div>
           </div>
         </div>
       </div>
